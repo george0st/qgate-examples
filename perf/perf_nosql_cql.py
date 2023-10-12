bf2eb71f29070125a255c19803c3fd72dabf3785
jist <95856749+george0st@users.noreply.github.com>
2023-10-12 08:50:03 +0200

    Tuning based on Function test

--- perf/perf_nosql_cql.py
+++ perf/perf_nosql_cql.py
@@ -7,11 +7,9 @@ from cassandra.query import BatchStatement
 from qgate_perf.parallel_executor import ParallelExecutor
 from qgate_perf.parallel_probe import ParallelProbe
 from qgate_perf.run_setup import RunSetup
-from qgate_perf.run_return import RunReturn
-import time
 import pandas, numpy
 
-def prf_Scylla(run_setup: RunSetup):
+def prf_cql(run_setup: RunSetup) -> ParallelProbe:
     """ Function for performance testing"""
     generator = numpy.random.default_rng()
 
@@ -20,8 +18,58 @@ def prf_Scylla(run_setup: RunSetup):
 
     profile = ExecutionProfile(request_timeout=60)
 
-    cluster = Cluster(contact_points=["localhost"],
-                      port=9042,
+    cluster = Cluster(contact_points=[run_setup.param('ip')],
+                      port=run_setup.param('port'),
+                      execution_profiles={EXEC_PROFILE_DEFAULT: profile},
+                      control_connection_timeout=60,
+                      idle_heartbeat_interval=60,
+                      connect_timeout=60)
+#    session = cluster.connect(keyspace="jist")
+    session = cluster.connect()
+
+    columns="fn0"
+    items="?"
+    for i in range(1, run_setup.bulk_col):
+        columns+=f", fn{i}"
+        items+=", ?"
+
+    insert_user = session.prepare(f"INSERT INTO jist.t01 ({columns}) VALUES ({items})")
+    batch = BatchStatement(consistency_level=ConsistencyLevel.ONE)
+
+    while (True):
+        batch.clear()
+
+        # insert, update only 1 mil. value, default limit for killing Cassandra solution
+        data_frm = pandas.DataFrame(generator.integers(999999, size=(run_setup.bulk_row, run_setup.bulk_col)),
+            columns=[f"fn{i}" for i in range(run_setup.bulk_col)])
+
+        # prepare data
+        for row in data_frm.values:
+            batch.add(insert_user, row)
+
+        # START - probe, only for this specific code part
+        probe.start()
+
+        session.execute(batch)
+
+        # STOP - probe
+        if probe.stop():
+            break
+
+    # RETURN - data from probe
+    return probe
+
+def prf_Scylla(run_setup: RunSetup) -> ParallelProbe:
+    """ Function for performance testing"""
+    generator = numpy.random.default_rng()
+
+    # INIT - contain executor synchonization, if needed
+    probe=ParallelProbe(run_setup)
+
+    profile = ExecutionProfile(request_timeout=60)
+
+    cluster = Cluster(contact_points=[run_setup.param('ip')],
+                      port=run_setup.param('port'),
                       execution_profiles={EXEC_PROFILE_DEFAULT: profile},
                       control_connection_timeout=60,
                       idle_heartbeat_interval=60,
@@ -29,9 +77,9 @@ def prf_Scylla(run_setup: RunSetup):
     session = cluster.connect(keyspace="catalog")
 
 
-    columns="fn0, fn1"
-    items="?, ?"
-    for i in range(2, run_setup.bulk_col):
+    columns="fn0"
+    items="?"
+    for i in range(1, run_setup.bulk_col):
         columns+=f", fn{i}"
         items+=", ?"
 
@@ -62,25 +110,27 @@ def prf_Scylla(run_setup: RunSetup):
     return probe
 
 
-def prf_Cassandra(run_setup: RunSetup):
+def prf_Cassandra(run_setup: RunSetup) -> ParallelProbe:
     """ Function for performance testing"""
     generator = numpy.random.default_rng()
 
     # INIT - contain executor synchonization, if needed
     probe=ParallelProbe(run_setup)
 
+
+
     profile = ExecutionProfile(request_timeout=60)
-    cluster = Cluster(['10.19.135.161'],
+    cluster = Cluster([run_setup.param('ip')],
                       execution_profiles={EXEC_PROFILE_DEFAULT: profile},
-                      port=9042,
+                      port=run_setup.param('port'),
                       control_connection_timeout=20,
                       idle_heartbeat_interval=60,
                       connect_timeout=20)
     session = cluster.connect()
 
-    columns="fn0, fn1"
-    items="?, ?"
-    for i in range(2, run_setup.bulk_col):
+    columns="fn0"
+    items="?"
+    for i in range(1, run_setup.bulk_col):
         columns+=f", fn{i}"
         items+=", ?"
 
@@ -129,6 +179,7 @@ def prepare_model():
         session.execute("CREATE TABLE IF NOT EXISTS jist.t01 (fn0 int PRIMARY KEY, fn1 int, fn2 int, fn3 int"
                         ", fn4 int, fn5 int, fn6 int, fn7 int, fn8 int, fn9 int)")
 
+        # init insert for basic tests
         for i in range(10):
             session.execute(f"INSERT INTO jist.t01 (fn0, fn1, fn2, fn3, fn4, fn5, fn6, fn7, fn8, fn9) "
                             f"VALUES({i},0, 0,0,0,0,0,0,0,0)")
@@ -141,21 +192,21 @@ def prepare_model():
 def perf_test(scylla: bool = False):
 
     if scylla:
-        generator = ParallelExecutor(prf_Scylla,
-                                     label="Scylla_impact",
+        generator = ParallelExecutor(prf_cql,
+                                     label="Scylla",
                                      detail_output=True,
-                                     output_file="prf_Scylla_01.txt")
-        setting={"port": 9042,
-                "ip":   "localhost"}
+                                     output_file="../output/prf_Scylla.txt")
+        setting={"ip":   "localhost",
+                 "port": 9042}
     else:
-        generator = ParallelExecutor(prf_Cassandra,
-                                     label="Cassandra_impact",
+        generator = ParallelExecutor(prf_cql,
+                                     label="Cassandra",
                                      detail_output=True,
-                                     output_file="prf_cassandara_01.txt")
-        setting={"port": 9042,
-                "ip":   "10.19.135.161"}
+                                     output_file="../output/prf_cassandara.txt")
+        setting={"ip":   "10.19.135.161",
+                 "port": 9042}
 
-    setup = RunSetup(duration_second=30, start_delay=15, parameters=setting)
+    setup = RunSetup(duration_second=30, start_delay=2, parameters=setting)
     generator.run_bulk_executor(bulk_list=[[200, 10]],
                                 executor_list=[[1, 2, '2x threads'],
                                                [4, 2, '2x threads'],
@@ -165,4 +216,4 @@ def perf_test(scylla: bool = False):
 
 if __name__ == '__main__':
     # prepare_model()
-    perf_test(False)
+    perf_test(True)
