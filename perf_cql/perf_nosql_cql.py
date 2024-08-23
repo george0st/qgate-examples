9e8a4723b7cc0b53d0e89deca168d9b01a336743
jist <95856749+george0st@users.noreply.github.com>
2024-08-23 19:24:35 +0200

    Update perf_nosql_cql.py

--- perf_cql/perf_nosql_cql.py
+++ perf_cql/perf_nosql_cql.py
@@ -249,8 +249,11 @@ if __name__ == '__main__':
     # list of executors (for application to all bulks)
     # executors = [[2, 1, '1x threads'], [4, 1, '1x threads'], [8, 1, '1x threads'],
     #              [2, 2, '2x threads'], [4, 2, '2x threads'], [8, 2, '2x threads']]
+    #
+    executors = [[2, 1, '1x threads'], [4, 1, '1x threads'], [8, 1, '1x threads'], [16, 1, '1x threads'],
+                 [2, 2, '2x threads'], [4, 2, '2x threads'], [8, 2, '2x threads'], [16, 2, '2x threads']]
 
-    executors = [[2, 1, '1x threads'], [2, 2, '2x threads']]
+    #executors = [[2, 1, '1x threads'], [4, 1, '1x threads']]
 
     # performance test duration
     duration_seconds=5
@@ -258,8 +261,10 @@ if __name__ == '__main__':
     config = dotenv_values("config/perf_nosql_cql.env")
     param=config.get('MULTIPLE_ENV', None)
     if param:
+        # multiple configurations
         envs=config["MULTIPLE_ENV"].split(",")
         for env in envs:
             exec_config(dotenv_values(env.strip()))
     else:
+        # single configuration
         exec_config(config)
