83ae3bc86b8da33db96f61b0062eebeebb9703e1
jist <95856749+george0st@users.noreply.github.com>
2024-08-25 15:58:36 +0200

    Update perf_nosql_cql.py

--- perf_cql/perf_nosql_cql.py
+++ perf_cql/perf_nosql_cql.py
@@ -121,7 +121,7 @@ def prf_cql_read(run_setup: RunSetup) -> ParallelProbe:
         while True:
 
             # generate synthetic data
-            #  NOTE: It will generate only values for two columns (as primary keys)
+            #  NOTE: It will generate only values for two columns (as primary keys), not for all columns
             synthetic_data = generator.integers(Setting.MAX_GNR_VALUE, size=(run_setup.bulk_row, 2))
 
             # START - probe, only for this specific code part
@@ -201,8 +201,6 @@ def prepare_model(cluster, run_setup: RunSetup):
                 session.execute(f"DROP KEYSPACE IF EXISTS {run_setup['keyspace']}")
 
                 # Create key space
-                # use different replication strategy 'class':'NetworkTopologyStrategy' for production HA mode
-                #            session.execute("CREATE KEYSPACE IF NOT EXISTS jist2 WITH replication = {'class':'NetworkTopologyStrategy', 'replication_factor' : 3};")
                 session.execute(f"CREATE KEYSPACE IF NOT EXISTS {run_setup['keyspace']}" +
                                 " WITH replication = {" +
                                 f"'class':'{run_setup['replication_class']}', 'replication_factor' : {run_setup['replication_factor']}" +
