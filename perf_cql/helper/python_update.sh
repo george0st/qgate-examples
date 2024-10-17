01751bd529669b7a86c142a53c486db97b5a84c7
jist <95856749+george0st@users.noreply.github.com>
2024-10-17 16:34:41 +0200

    Update python_update.sh

--- perf_cql/helper/python_update.sh
+++ perf_cql/helper/python_update.sh
@@ -5,3 +5,4 @@ python3.11 -m pip install qgate-perf==0.4.36
 python3.11 -m pip install cassandra-driver==3.29.2
 python3.11 -m pip install numpy==2.1.1
 python3.11 -m pip install matplotlib==3.9.2
+python3.11 -m pip install python-dotenv==1.0
