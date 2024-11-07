e500545a6bfa891e3cb473b393f2ee108de104e8
jist <95856749+george0st@users.noreply.github.com>
2024-11-07 18:40:33 +0100

    Add ENV

--- perf_cql/helper/perf_fin__nonprod_run.sh
+++ perf_cql/helper/perf_fin__nonprod_run.sh
@@ -4,3 +4,4 @@
 # Run without terminal
 ###########################
 nohup python3.11 perf_cql/perf_cql.py run -e _fin_nonprod.env -d perf_cql
+nohup python3.11 perf_cql/perf_cql.py run -e _fin_nonprod_peek.env -d perf_cql
