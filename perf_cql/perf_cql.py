1424a10a5102389500a52740c8dc98db82b47d1f
jist <95856749+george0st@users.noreply.github.com>
2024-11-10 13:58:42 +0100

    Tune output info

--- perf_cql/perf_cql.py
+++ perf_cql/perf_cql.py
@@ -312,9 +312,10 @@ def main_execute(multi_env="cass.env", perf_dir = ".", only_cluster_diagnose = F
                 state = False
             count_states += output.count_states
             count_false_states += output.count_false_states
-        print(Fore.LIGHTGREEN_EX + f"=> FINISH tests ("
-                                   f"ENV files: {len(envs)}, "
-                                   f"Total state: {'OK' if state else 'Err'} [Count: {count_states}, ERR count: {count_false_states}]) <=" + Style.RESET_ALL)
+
+        print(Fore.LIGHTGREEN_EX + f"=> FINISH tests, "
+                                   f"State: {'OK' if state else 'Err'} (Count: {count_states}"
+                                   f"{', ERR count: '+str(count_false_states) if count_false_states > 0 else ''})) <=" + Style.RESET_ALL)
     else:
         print(Fore.LIGHTRED_EX + "!!! Missing 'MULTIPLE_ENV' configuration !!!" + Style.RESET_ALL)
 
