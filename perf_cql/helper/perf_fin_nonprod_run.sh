62e403ef0ff7476a28ed22e5284035db99a5e06a
jist <95856749+george0st@users.noreply.github.com>
2024-11-11 21:05:34 +0100

    Add TS01 RW

--- perf_cql/helper/perf_fin_nonprod_run.sh
+++ perf_cql/helper/perf_fin_nonprod_run.sh
@@ -6,3 +6,4 @@
 nohup python3.11 perf_cql/perf_cql.py run -e _fin_nonprod_ts01_init.env -d perf_cql
 nohup python3.11 perf_cql/perf_cql.py run -e _fin_nonprod_ts01_write.env -d perf_cql
 nohup python3.11 perf_cql/perf_cql.py run -e _fin_nonprod_ts01_read.env -d perf_cql
+nohup python3.11 perf_cql/perf_cql.py run -e _fin_nonprod_ts01_readwrite.env -d perf_cql
