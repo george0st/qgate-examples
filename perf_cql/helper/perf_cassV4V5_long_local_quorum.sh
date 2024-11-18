f4ee959733ca4f404224adf0c847ebf241303afd
jist <95856749+george0st@users.noreply.github.com>
2024-11-18 21:21:49 +0100

    ...

---
+++ perf_cql/helper/perf_cassV4V5_long_local_quorum.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Loaded data for test
+#  - ~90 mil items in each table
+####################################
+
+# test Cassandra V4
+python3.11 perf_cql/perf_cql.py run -e _cassV4_write.env -d perf_cql -fp "CONSISTENCY_LEVEL = LOCAL_QUORUM;NUMERIC_SCOPE = 7500;"
+python3.11 perf_cql/perf_cql.py run -e _cassV4_read.env -d perf_cql -fp "CONSISTENCY_LEVEL = LOCAL_QUORUM;NUMERIC_SCOPE = 7500;"
+python3.11 perf_cql/perf_cql.py run -e _cassV4_readwrite.env -d perf_cql -fp "CONSISTENCY_LEVEL = LOCAL_QUORUM;NUMERIC_SCOPE = 7500;"
+
+# test Cassandra V5
+python3.11 perf_cql/perf_cql.py run -e _cassV5_write.env -d perf_cql -fp "CONSISTENCY_LEVEL = LOCAL_QUORUM;NUMERIC_SCOPE = 7500;"
+python3.11 perf_cql/perf_cql.py run -e _cassV5_read.env -d perf_cql -fp "CONSISTENCY_LEVEL = LOCAL_QUORUM;NUMERIC_SCOPE = 7500;"
+python3.11 perf_cql/perf_cql.py run -e _cassV5_readwrite.env -d perf_cql -fp "CONSISTENCY_LEVEL = LOCAL_QUORUM;NUMERIC_SCOPE = 7500;"
